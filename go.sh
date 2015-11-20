d9d221e14c1b2a54eb4ab9ebb195b320e713997d
Geoff Howland <ghowland@Geoffs-MacBook-Air.local>
2015-11-20 04:45:17 -0800

    Updated

--- go.sh
+++ go.sh
@@ -3,5 +3,5 @@
 # Shortcut to update things and store in git.  Fragile.
 #
 
-./make_book.py && git add sections/* && git commit -m "Updated" -a > /dev/null && git push -u origin master > /dev/null
+./make_book.py && git add sections/* > /dev/null && git commit -m "Updated" -a > /dev/null && git push -u origin master > /dev/null
 
