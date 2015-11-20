dc83458410de625c1b37c5ba3e7b774a268a9cea
Geoff Howland <geoff@gmail.com>
2015-11-20 05:15:41 -0800

    Updated

--- go.sh
+++ go.sh
@@ -3,6 +3,5 @@
 # Shortcut to update things and store in git.  Fragile.
 #
 
-./make_book.py && git add __toc_backups/* > /dev/null && git add sections/* > /dev/null && git commit -m "Updated" -a > /dev/null && git push -u origin master > /dev/null
+./make_book.py && git add __toc_backups/* > /dev/null && git add sections/* > /dev/null && git commit -m "Updated" -a > /dev/null && git push -u origin master > /dev/null && exit 0
 
-exit 0
